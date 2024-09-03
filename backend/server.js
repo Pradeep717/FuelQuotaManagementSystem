26e4a715c608d9b37602f6f77099a3a1ae17f76c
pradeep-s717 <149034881+Pradeep-s717@users.noreply.github.com>
2024-09-03 21:44:40 +0530

    Project initialization

---
+++ backend/server.js
@@ -0,0 +1,9 @@
+import express from 'express';
+import dotenv from 'dotenv';
+ 
+dotenv.config();
+const app = express();
+
+const PORT = process.env.PORT || 5000;
+
+app.listen(500, () => console.log(`Server is running on port ${PORT}`));
