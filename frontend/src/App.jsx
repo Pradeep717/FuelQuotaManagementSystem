abd1a775af31e410b00912aa8525767fc117d002
Asanka Madushan <85320753+NnAsankaMadushan@users.noreply.github.com>
2024-09-05 22:21:05 +0530

    added login page

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,14 +1,19 @@
 import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
+import React from 'react';
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import './App.css'
 import Start from './Components/Start/Start'
-// import WithSubnavigation from './Components/Navigation/WithSubnavigation'
-
+import Login from './Components/Login/Login'
 function App() {
-  const [count, setCount] = useState(0)
   return (
-    <Start/>
+    <Router>
+    <Routes>
+      <Route path="/" element={<Start />} />
+      <Route path="/login" element={<Login />} />
+    </Routes>
+  </Router>
   )
 }
 
--- frontend/src/Components/Start/Start.jsx
+++ frontend/src/Components/Start/Start.jsx
@@ -1,36 +1,45 @@
 import React from 'react';
 import './Start.css';
+import { Link } from 'react-router-dom';
+// import NavigationBar from '../Navigation/NavigationBar';
+
 
 const Start = () => {
   return (
-    <div className="container">
-      <div className="textSection">
-        <h1 className="title">
-          Fuel<span className='highlightedText'>Plus</span> Station!
-        </h1>
-        <h2 className="description">
-        "Fuel up for the journey, service that goes the extra mile!"
-        </h2>
-        <p>FuelPlus Station is your one-stop destination for all your vehicle needs. We offer a range of services, including fuel refills, oil changes, tire checks, and car wash facilities. Our friendly staff ensures quick and efficient service, so you can get back on the road in no time. Drive in today and experience the difference with FuelPlus Station, where quality service meets convenience.
-</p>
-        <div className="buttonContainer">
-          <button className="getStartedButton">Log In</button>
-          <button className="howItWorksButton">Sign Up</button>
+    <div className="start-container">
+      {/* <NavigationBar/> */}
+      <div className="container">
+        <div className="textSection">
+          <h1 className="title">
+            Fuel<span className='highlightedText'>Plus</span> Station!
+          </h1>
+          <h2 className="description">
+            
+          "Fuel up for the journey, service that goes the extra mile!"
+          </h2>
+          <p>FuelPlus Station is your one-stop destination for all your vehicle needs. We offer a range of services, including fuel refills, oil changes, tire checks, and car wash facilities. Our friendly staff ensures quick and efficient service, so you can get back on the road in no time. Drive in today and experience the difference with FuelPlus Station, where quality service meets convenience.
+  </p>
+          <div className="buttonContainer">
+           <Link to="/login">
+              <button className="getStartedButton">Log In</button>
+           </Link>
+            <button className="howItWorksButton">Sign Up</button>
+          </div>
+          <div className='social'>
+              <img src="https://img.icons8.com/?size=100&id=118498&format=png&color=000000" alt="Facebook" style={{ width: '40px', height: '40px' }} />
+              <img src="https://img.icons8.com/?size=100&id=3lrBZiVC31vI&format=png&color=000000" alt="Instagram" style={{ width: '45px', height: '45px' }} />
+              <img src="https://img.icons8.com/?size=100&id=qbiAUnUMOnLp&format=png&color=000000" alt="Email" style={{ width: '40px', height: '40px' }} />
+              <img src="https://img.icons8.com/?size=100&id=42961&format=png&color=000000" alt="Whatsapp" style={{ width: '40px', height: '40px' }} />
+          </div>
         </div>
-        <div className='social'>
-            <img src="https://img.icons8.com/?size=100&id=118498&format=png&color=000000" alt="Facebook" style={{ width: '40px', height: '40px' }} />
-            <img src="https://img.icons8.com/?size=100&id=3lrBZiVC31vI&format=png&color=000000" alt="Instagram" style={{ width: '45px', height: '45px' }} />
-            <img src="https://img.icons8.com/?size=100&id=qbiAUnUMOnLp&format=png&color=000000" alt="Email" style={{ width: '40px', height: '40px' }} />
-            <img src="https://img.icons8.com/?size=100&id=42961&format=png&color=000000" alt="Whatsapp" style={{ width: '40px', height: '40px' }} />
+        <div className="imageSection">
+          <div className="imageWrapper">
+              <img src="/src/assets/Images/shape.png" alt="Shape" className="shape" />
+              <img src="/src/assets/Images/petrol.png" alt="Petrol" className="petrol" />
+          </div>
         </div>
+        
       </div>
-      <div className="imageSection">
-        <div className="imageWrapper">
-            <img src="/src/assets/Images/shape.png" alt="Shape" className="shape" />
-            <img src="/src/assets/Images/petrol.png" alt="Petrol" className="petrol" />
-        </div>
-      </div>
-      
     </div>
   );
 };
