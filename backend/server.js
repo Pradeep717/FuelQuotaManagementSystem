2e7b8d00e8a4ea6a1786cb6047891f4789d34a88
pradeep-s717 <149034881+Pradeep-s717@users.noreply.github.com>
2024-10-09 17:43:08 +0530

    Add fuel transaction route

--- backend/server.js
+++ backend/server.js
@@ -3,6 +3,7 @@ import dotenv from 'dotenv';
 import connectDB from './db/connectDB.js';
 import userRoutes from './routes/userRoutes.js';
 import vehicleRoutes from './routes/vehicleRoutes.js';
+import fuelTransactionRoutes from './routes/fuelTransactionRoutes.js';
 import cookieParser from 'cookie-parser';
 import cors from 'cors';
 
@@ -23,6 +24,7 @@ app.use(cookieParser());
 // Routes
 app.use('/api/users', userRoutes);
 app.use('/api/vehicles', vehicleRoutes);
+app.use('/api/fuel', fuelTransactionRoutes);
 app.use('/', (req, res) => res.send('API is running...'));
 // app.use('/api/vehicles', vehicleRoutes);
 
